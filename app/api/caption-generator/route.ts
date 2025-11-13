bcaa910a6b842bf9acd16a5b3435f034d70a3bc0
MRX <mrxmrx459@gmail.com>
2025-11-13 23:45:03 +0800

    fix(caption_gen): change ai model

--- app/api/caption-generator/route.ts
+++ app/api/caption-generator/route.ts
@@ -9,7 +9,7 @@ if (!API_KEY) {
 
 const genAI = new GoogleGenerativeAI(API_KEY || "");
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.5-flash",
+  model: "gemini-2.0-flash",
   generationConfig: {
     temperature: 1.5,
     maxOutputTokens: 8192,
